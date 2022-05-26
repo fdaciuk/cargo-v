ffd409a5fdc4e1ab0e366f798e05bd90877a4311
Fernando Daciuk <f.daciuk@gmail.com>
2022-05-25 21:25:42 -0300

    feat: return u32 instead of String from tuple

--- src/lib.rs
+++ src/lib.rs
@@ -9,30 +9,31 @@ pub fn update_version_by_label(
   label: VersionLabel,
 ) -> String {
   let current_version = get_version(&cargo_toml_content);
-  let major: i32 = current_version.0.parse().unwrap();
-  let minor: i32 = current_version.1.parse().unwrap();
-  let patch: i32 = current_version.2.parse().unwrap();
-
+  let (major, minor, patch) = current_version;
   let new_version = match label {
     VersionLabel::Major => format!("{}.0.0", major + 1),
     VersionLabel::Minor => format!("{}.{}.0", major, minor + 1),
     VersionLabel::Patch => format!("{}.{}.{}", major, minor, patch + 1),
   };
 
-  cargo_toml_content.replace(&tuple_to_string(current_version), &new_version)
+  cargo_toml_content
+    .replace(&tuple_version_to_string(current_version), &new_version)
 }
 
 pub fn update_version(cargo_toml_content: String, version: String) -> String {
   let current_version_tuple = get_version(&cargo_toml_content);
-  let current_version = tuple_to_string(current_version_tuple);
+  let current_version = tuple_version_to_string(current_version_tuple);
   cargo_toml_content.replace(&current_version, &version)
 }
 
-fn tuple_to_string(tuple_version: (String, String, String)) -> String {
-  tuple_version.0 + "." + &tuple_version.1 + "." + &tuple_version.2
+fn tuple_version_to_string(tuple_version: (u32, u32, u32)) -> String {
+  format!(
+    "{}.{}.{}",
+    tuple_version.0, tuple_version.1, tuple_version.2,
+  )
 }
 
-fn get_version(cargo_toml_content: &str) -> (String, String, String) {
+fn get_version(cargo_toml_content: &str) -> (u32, u32, u32) {
   let version = cargo_toml_content
     .lines()
     .filter(|line| line.contains("version"))
@@ -50,9 +51,9 @@ fn get_version(cargo_toml_content: &str) -> (String, String, String) {
     version.expect("Your Cargo.toml file does not have a \"version\" entry");
 
   let mut version_split = version.split('.');
-  let major = version_split.next().unwrap().to_owned();
-  let minor = version_split.next().unwrap().to_owned();
-  let patch = version_split.next().unwrap().to_owned();
+  let major = version_split.next().unwrap().parse().unwrap();
+  let minor = version_split.next().unwrap().parse().unwrap();
+  let patch = version_split.next().unwrap().parse().unwrap();
   (major, minor, patch)
 }
 
@@ -81,7 +82,9 @@ mod tests {
   }
 
   #[test]
-  #[ignore]
+  #[should_panic(
+    expected = "Your Cargo.toml file does not have a \"version\" entry"
+  )]
   fn should_fail_if_cargo_toml_does_not_have_version() {
     let cargo_toml = String::from("[package]\n name = \"cargo-v\"\n");
     let new_version = String::from("0.1.0");
