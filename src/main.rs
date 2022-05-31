63d9a4cbedbb6978785aca4f0a0e31ed5b5aa6cd
Fernando Daciuk <f.daciuk@gmail.com>
2022-05-31 08:27:00 -0300

    feat: refactoring and read from env args and run some command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
-use std::{env, fs};
+use cargo_v;
+use std::{env, fs, process::Command};
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  dbg!(args);
+  let mut args = env::args();
+  args.next();
 
-  let file = fs::read_to_string("./Cargo.toml");
-  match file {
-    Ok(data) => println!("{data}"),
-    Err(err) => println!("{err}"),
+  let new_version_input = args
+    .next()
+    .expect("You must pass the version (patch, minor, major)");
+
+  let file =
+    fs::read_to_string("./Cargo.toml").expect("Can't read Cargo.toml file.");
+
+  let new_version_enum = match new_version_input.as_str() {
+    "patch" => cargo_v::VersionLabel::Patch,
+    "minor" => cargo_v::VersionLabel::Minor,
+    "major" => cargo_v::VersionLabel::Major,
+    v => cargo_v::VersionLabel::NumericVersion(String::from(v)),
   };
+
+  let cargo_toml_updated = cargo_v::update_version(&file, new_version_enum);
+  let new_version = cargo_v::get_version(&cargo_toml_updated);
+  let new_version = cargo_v::tuple_version_to_string(new_version);
+
+  git_add();
+  git_commit(&new_version);
+  git_tag(&new_version);
+}
+
+fn git_add() {
+  let _ = Command::new("echo").args(["git add ."]).spawn();
+}
+
+fn git_commit(version: &str) {
+  let _ = Command::new("echo")
+    .args([format!("git commit -m 'v{}'", version)])
+    .spawn();
+}
+
+fn git_tag(version: &str) {
+  let _ = Command::new("echo")
+    .args([format!("git tag -a v{v} -m 'v{v}'", v = version)])
+    .spawn();
 }
