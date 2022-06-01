7a8b0b0da2cbae1b5f6443088584a78d7be1600c
Fernando Daciuk <f.daciuk@gmail.com>
2022-06-01 08:13:54 -0300

    fix: switch .spawn to .output to wait the command to finish

--- src/main.rs
+++ src/main.rs
@@ -27,37 +27,44 @@ fn main() {
   let new_version = cargo_v::get_version(&cargo_toml_updated);
   let new_version = cargo_v::tuple_version_to_string(&new_version);
 
-  if update_cargo_files(&cargo_toml_updated).is_err() {
-    println!("Error trying to write on file Cargo.toml");
+  if update_cargo_toml(&cargo_toml_updated).is_err() {
+    eprintln!("Error trying to write on file Cargo.toml");
+    process::exit(1);
   }
+
+  run_build();
 
   git_add();
   git_commit(&new_version);
   git_tag(&new_version);
-  process::exit(0);
 }
 
-fn update_cargo_files(new_cargo_toml: &str) -> Result<(), Box<dyn Error>> {
+fn update_cargo_toml(new_cargo_toml: &str) -> Result<(), Box<dyn Error>> {
   fs::write("./Cargo.toml", new_cargo_toml)?;
-  Command::new("cargo").args(["build", "--release"]).spawn()?;
   Ok(())
+}
+
+fn run_build() {
+  Command::new("cargo")
+    .args(["build", "--release", "--quiet"])
+    .output()
+    .expect("Failed to build project.");
 }
 
 fn git_add() {
   let _ = Command::new("git")
     .args(["add", "Cargo.toml", "Cargo.lock"])
-    .spawn();
+    .output();
 }
 
 fn git_commit(version: &str) {
-  let version = &format!("'v{}'", version);
-  let _ = Command::new("git").args(["commit", "-m", version]).spawn();
+  let version = &format!("v{}", version);
+  let _ = Command::new("git").args(["commit", "-m", version]).output();
 }
 
 fn git_tag(version: &str) {
   let version = &format!("v{}", version);
-  let message = &format!("'{}'", version);
   let _ = Command::new("git")
-    .args(["tag", "-a", version, "-m", message])
-    .spawn();
+    .args(["tag", "-a", version, "-m", version])
+    .output();
 }
