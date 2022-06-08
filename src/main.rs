69ee5eef73b0df6607968f55a3ea40f006c5e515
Fernando Daciuk <f.daciuk@gmail.com>
2022-06-08 08:37:32 -0300

    feat: improve error handling on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use cargo_v;
-use std::error::Error;
-use std::process::{self, Command};
-use std::{env, fs};
+use std::process::Command;
+use std::{env, fs, io};
 
-fn main() -> Result<(), Box<dyn Error>> {
-  let mut args = env::args();
+fn main() {
+  let mut args = env::args().skip(2);
 
-  args.next();
-  args.next();
+  let new_version = match args.next() {
+    Some(v) => v,
+    None => error_exit(
+      "failed to read the version: you must pass (patch, minor, major, etc.)",
+    ),
+  };
 
-  let new_version_input = args
-    .next()
-    .expect("You must pass the version (patch, minor, major)");
+  let cargo_toml = match fs::read_to_string("./Cargo.toml") {
+    Ok(toml) => toml,
+    Err(e) => error_exit(&format!("failed to read Cargo.toml file: {}", e)),
+  };
 
-  let cargo_toml =
-    fs::read_to_string("./Cargo.toml").expect("Can't read Cargo.toml file.");
-
-  let new_version_enum = match new_version_input.as_str() {
+  let new_version = match new_version.as_str() {
     "patch" => cargo_v::VersionLabel::Patch,
     "minor" => cargo_v::VersionLabel::Minor,
     "major" => cargo_v::VersionLabel::Major,
@@ -24,50 +25,74 @@ fn main() -> Result<(), Box<dyn Error>> {
   };
 
   let cargo_toml_updated =
-    cargo_v::update_version(&cargo_toml, &new_version_enum)?;
+    match cargo_v::update_version(&cargo_toml, &new_version) {
+      Ok(toml) => toml,
+      Err(e) => error_exit(&e.to_string()),
+    };
 
-  let new_version = cargo_v::get_version(&cargo_toml_updated)?;
+  let new_version = match cargo_v::get_version(&cargo_toml_updated) {
+    Ok(v) => v,
+    Err(e) => error_exit(&e.to_string()),
+  };
   let new_version = cargo_v::tuple_version_to_string(&new_version);
 
-  if update_cargo_toml(&cargo_toml_updated).is_err() {
-    eprintln!("Error trying to write on file Cargo.toml");
-    process::exit(1);
+  if let Err(e) = update_cargo_toml(&cargo_toml_updated) {
+    error_exit(&format!("failed to write on file Cargo.toml: {e}"));
   }
 
-  run_build();
+  if let Err(e) = run_build() {
+    error_exit(&format!("failed to run build: {e}"));
+  }
 
-  git_add();
-  git_commit(&new_version);
-  git_tag(&new_version);
-  Ok(())
+  if let Err(e) = git_add() {
+    error_exit(&format!("failed to run git add: {e}"));
+  }
+
+  if let Err(e) = git_commit(&new_version) {
+    error_exit(&format!("failed to run git commit: {e}"));
+  }
+
+  if let Err(e) = git_tag(&new_version) {
+    error_exit(&format!("failed to run git tag: {e}"));
+  }
 }
 
-fn update_cargo_toml(new_cargo_toml: &str) -> Result<(), Box<dyn Error>> {
+fn error_exit(msg: &str) -> ! {
+  eprintln!("ERROR: {}", msg);
+  std::process::exit(1);
+}
+
+fn update_cargo_toml(new_cargo_toml: &str) -> io::Result<()> {
   fs::write("./Cargo.toml", new_cargo_toml)?;
   Ok(())
 }
 
-fn run_build() {
+fn run_build() -> io::Result<()> {
   Command::new("cargo")
     .args(["build", "--release"])
-    .output()
-    .expect("Failed to build project.");
+    .output()?;
+  Ok(())
 }
 
-fn git_add() {
-  let _ = Command::new("git")
+fn git_add() -> io::Result<()> {
+  Command::new("git")
     .args(["add", "Cargo.toml", "Cargo.lock"])
-    .output();
+    .output()?;
+  Ok(())
 }
 
-fn git_commit(version: &str) {
+fn git_commit(version: &str) -> io::Result<()> {
   let version = &format!("v{}", version);
-  let _ = Command::new("git").args(["commit", "-m", version]).output();
+  Command::new("git")
+    .args(["commit", "-m", version])
+    .output()?;
+  Ok(())
 }
 
-fn git_tag(version: &str) {
+fn git_tag(version: &str) -> io::Result<()> {
   let version = &format!("v{}", version);
-  let _ = Command::new("git")
+  Command::new("git")
     .args(["tag", "-a", version, "-m", version])
-    .output();
+    .output()?;
+  Ok(())
 }
